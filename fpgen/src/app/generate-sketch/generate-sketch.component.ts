48c437819179314f5ddf8dc50d27df84ae84908b
Amr Anwar <amranwar945@gmail.com>
2022-06-12 03:20:54 +0200

    feat: add canvas

--- fpgen/src/app/generate-sketch/generate-sketch.component.ts
+++ fpgen/src/app/generate-sketch/generate-sketch.component.ts
@@ -1,5 +1,7 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { I18nInterface } from 'ngx-image-drawing';
+import { fabric } from 'fabric';
+import { Line, Object } from 'fabric/fabric-impl';
 
 @Component({
   selector: 'app-generate-sketch',
@@ -7,34 +9,318 @@ import { I18nInterface } from 'ngx-image-drawing';
   styleUrls: ['./generate-sketch.component.scss'],
 })
 export class GenerateSketchComponent implements OnInit {
-  public locale: string = 'en';
-  public width = 1000;
-  public height = window.innerHeight - 600;
+  myCanvas: any;
+  image = new Image();
+  //url: string;
+  isCanvasDrawn: boolean = true;
+  canvas: any;
+  polygon: any;
+  isImageDrawn: boolean = false;
+  isPolygonDrawn: boolean = false;
+  points: any[] = [];
+  newPt: any;
 
-  public i18n: I18nInterface = {
-    saveBtn: 'Generate Image from Sketch!',
-    sizes: {
-      extra: 'Extra',
-    },
-  };
-
-  constructor() {
-    this.locale = this.getNavigatorLanguage();
-  }
+  fromx!: number;
+  fromy!: number;
+  constructor() {}
 
   ngOnInit(): void {
+    this.canvas = new fabric.Canvas('canvasID', { fireRightClick: true });
+    // this.polygon = new fabric.Polygon(this.points, {
+    //   left: 0,
+    //   top: 0,
+    //   fill: 'rgba(255,0,0,0.1)',
+    //   strokeWidth: 1,
+    //   stroke: 'lightgrey',
+    //   scaleX: 1,
+    //   scaleY: 1,
+    //   objectCaching: false,
+    //   transparentCorners: false,
+    //   cornerColor: 'blue',
+    // });
+    this.canvas.viewportTransform = [1, 0, 0, 1, 0, 0];
+  }
+  isDown!: boolean;
+  line!: Object;
+  public ngAfterViewInit() {
+    this.canvas.on('mouse:down', (o:any) => {
+      this.isDown = true;
+      var pointer = this.canvas.getPointer(o.e);
+      this.points = [
+        pointer.x,
+        pointer.y,
+        pointer.x,
+        pointer.y,
+      ];
 
+        let tox = pointer.x;
+      let toy = pointer.y;
+      this.fromx = tox -10;
+      this.fromy = toy - 10;
+        //this.drawArrow(startX, startY, endX, endY);
+
+        var angle = Math.atan2(toy - this.fromy, tox - this.fromx);
+
+        var headlen = 3; // arrow head size
+
+        // bring the line end back some to account for arrow head.
+        tox = tox - headlen * Math.cos(angle);
+        toy = toy - headlen * Math.sin(angle);
+
+        // calculate the points.
+        var newPoints = [
+          {
+            x: this.fromx, // start point
+            y: this.fromy,
+          },
+          {
+            x: this.fromx - (headlen / 4) * Math.cos(angle - Math.PI / 2),
+            y: this.fromy - (headlen / 4) * Math.sin(angle - Math.PI / 2),
+          },
+          {
+            x: tox - (headlen / 4) * Math.cos(angle - Math.PI / 2),
+            y: toy - (headlen / 4) * Math.sin(angle - Math.PI / 2),
+          },
+          {
+            x: tox - headlen * Math.cos(angle - Math.PI / 2),
+            y: toy - headlen * Math.sin(angle - Math.PI / 2),
+          },
+          {
+            x: tox + headlen * Math.cos(angle), // tip
+            y: toy + headlen * Math.sin(angle),
+          },
+          {
+            x: tox - headlen * Math.cos(angle + Math.PI / 2),
+            y: toy - headlen * Math.sin(angle + Math.PI / 2),
+          },
+          {
+            x: tox - (headlen / 4) * Math.cos(angle + Math.PI / 2),
+            y: toy - (headlen / 4) * Math.sin(angle + Math.PI / 2),
+          },
+          {
+            x: this.fromx - (headlen / 4) * Math.cos(angle + Math.PI / 2),
+            y: this.fromy - (headlen / 4) * Math.sin(angle + Math.PI / 2),
+          },
+          {
+            x: this.fromx,
+            y: this.fromy,
+          },
+        ];
+
+        this.line = new fabric.Polyline(newPoints, {
+          fill: 'black', //'white',
+          stroke: 'black', //'black',
+          opacity: 1,
+          strokeWidth: 1,
+          originX: 'left',
+          originY: 'top',
+          selectable: true,
+        });
+
+      ////////////
+      // var triangle = new fabric.Triangle({
+      //   width: 10,
+      //   height: 10,
+      //   fill: 'black',
+      //   left: pointer.x,
+      //   top: pointer.y,
+      //   angle: 200,
+      // });
+
+      //   var line = new fabric.Line(this.points, {
+      //       strokeWidth: 2,
+      //       fill: 'black',
+      //       stroke: 'black',
+      //       originX: 'left',
+      //       originY: 'center',
+      //     // left: 75,
+      //     // strokeWidth: 1,
+      //     // width: 1,
+      //     // height: 1,
+      //     // top: 70,
+      //     // stroke: 'black',
+      //   });
+
+      // var objs = [line, triangle];
+
+      // this.line  = new fabric.Group(objs);
+      // this.line = new fabric.Line(this.points, {
+      //   strokeWidth: 2,
+      //   fill: 'black',
+      //   stroke: 'black',
+      //   originX: 'left',
+      //   originY: 'center',
+      // });
+      this.canvas.add(this.line);
+    });
+
+    this.canvas.on('mouse:move', (o:any) => {
+      if (!this.isDown) return;
+      var pointer = this.canvas.getPointer(o.e);
+
+      var startX = this.points[0];
+      var startY = this.points[1];
+      var x2 = pointer.x - startX;
+      var y2 = pointer.y - startY;
+      var r = Math.sqrt(x2 * x2 + y2 * y2);
+      var angle = (Math.atan2(y2, x2) / Math.PI) * 180;
+
+      angle = (angle % 360) + 180;
+
+      // if (angle <= 22.5 || angle >= 337.5) {
+      //   angle = 0;
+      // } else if (angle <= 67.5) {
+      //   angle = 45;
+      // } else if (angle <= 112.5) {
+      //   angle = 90;
+      // } else if (angle <= 157.5) {
+      //   angle = 135;
+      // } else if (angle <= 202.5) {
+      //   angle = 180;
+      // } else if (angle <= 247.5) {
+      //   angle = 225;
+      // } else if (angle <= 292.5) {
+      //   angle = 270;
+      // } else if (angle < 337.5) {
+      //   angle = 315;
+      // }
+      angle -= 180;
+
+      var cosx = r * Math.cos((angle * Math.PI) / 180);
+      var sinx = r * Math.sin((angle * Math.PI) / 180);
+
+      this.line.set({
+        angle: angle,
+        // x2: cosx + startX,
+        // y2: sinx + startY,
+      });
+      this.canvas.renderAll();
+    });
+
+    this.canvas.on('mouse:up', () => {
+      this.isDown = false;
+    });
+    // this.canvas.on('mouse:up', (event: any) => {
+    //   console.log('MOUSE UP');
+    //   var pointer = this.canvas.getPointer(event.e);
+    //   let tox = pointer.x;
+    //   let toy = pointer.y;
+    //   //this.drawArrow(startX, startY, endX, endY);
+
+    //   var angle = Math.atan2(toy - this.fromy, tox - this.fromx);
+    //   tox = this.fromx+ 4;
+    //   toy = this.fromy + angle * 4;
+    //   console.log("ANGEL: ", angle)
+    //   var headlen = 10; // arrow head size
+
+    //   // bring the line end back some to account for arrow head.
+    //   tox = tox - headlen * Math.cos(angle);
+    //   toy = toy - headlen * Math.sin(angle);
+
+    //   // calculate the points.
+    //   var points = [
+    //     {
+    //       x: this.fromx, // start point
+    //       y: this.fromy,
+    //     },
+    //     {
+    //       x: this.fromx - (headlen / 4) * Math.cos(angle - Math.PI / 2),
+    //       y: this.fromy - (headlen / 4) * Math.sin(angle - Math.PI / 2),
+    //     },
+    //     {
+    //       x: tox - (headlen / 4) * Math.cos(angle - Math.PI / 2),
+    //       y: toy - (headlen / 4) * Math.sin(angle - Math.PI / 2),
+    //     },
+    //     {
+    //       x: tox - headlen * Math.cos(angle - Math.PI / 2),
+    //       y: toy - headlen * Math.sin(angle - Math.PI / 2),
+    //     },
+    //     {
+    //       x: tox + headlen * Math.cos(angle), // tip
+    //       y: toy + headlen * Math.sin(angle),
+    //     },
+    //     {
+    //       x: tox - headlen * Math.cos(angle + Math.PI / 2),
+    //       y: toy - headlen * Math.sin(angle + Math.PI / 2),
+    //     },
+    //     {
+    //       x: tox - (headlen / 4) * Math.cos(angle + Math.PI / 2),
+    //       y: toy - (headlen / 4) * Math.sin(angle + Math.PI / 2),
+    //     },
+    //     {
+    //       x: this.fromx - (headlen / 4) * Math.cos(angle + Math.PI / 2),
+    //       y: this.fromy - (headlen / 4) * Math.sin(angle + Math.PI / 2),
+    //     },
+    //     {
+    //       x: this.fromx,
+    //       y: this.fromy,
+    //     },
+    //   ];
+
+    //   var pline = new fabric.Polyline(points, {
+    //     fill: 'black', //'white',
+    //     stroke: 'black', //'black',
+    //     opacity: 1,
+    //     strokeWidth: 1,
+    //     originX: 'left',
+    //     originY: 'top',
+    //     selectable: true,
+    //   });
+
+    //   this.canvas.add(pline);
+
+    //   // this.isDown = false;
+    //   // this.canvas.off('mouse:down').off('mouse:move').off('mouse:up');
+
+    //   this.canvas.renderAll();
+    // });
+
+    // this.canvas.on('mouse:down', (event: any) => {
+    //   console.log('MOUSE down');
+    //   var pointer = this.canvas.getPointer(event.e);
+    //   this.fromx = pointer.x;
+    //   this.fromy = pointer.y;
+    //   // this.generateArrow()
+    // });
+  }
+  getClickCoords(event: any) {
+    if (this.isCanvasDrawn && this.isImageDrawn) {
+      this.newPt = {
+        x: event.layerX,
+        y: event.layerY,
+      };
+      // this.points.push(this.newPt);
+      this.canvas.add(this.polygon);
+    }
+  }
+  public generateArrow() {
+    console.log('FREGERGddd');
+
+    var triangle = new fabric.Triangle({
+      width: 10,
+      height: 15,
+      fill: 'black',
+      left: 50,
+      top: 10,
+      angle: 90,
+    });
+
+    var line = new fabric.Line([50, 10, 200, 10], {
+      left: 75,
+      strokeWidth: 1,
+      width: 1,
+      height: 1,
+      top: 70,
+      stroke: 'black',
+    });
+
+    var objs = [line, triangle];
+
+    var alltogetherObj = new fabric.Group(objs);
+    this.canvas.add(alltogetherObj);
   }
   public saveBtn($event: any) {
     // saveAs($event, 'image.jpg');
     //TODO: call backend server
   }
-
-  private getNavigatorLanguage = () =>
-    navigator.languages && navigator.languages.length
-      ? navigator.languages[0]
-      : (navigator as any).userLanguage ||
-        navigator.language ||
-        (navigator as any).browserLanguage ||
-        'en';
 }
